95f6f5b65e3b3cee7d8740d3b01e6f154506323d
AbuUsaid <usaidabu321@gmail.com>
2023-04-23 19:54:01 +0530

    Add 01folder and 02folder past changes

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return 'ERROR';
+  } else {
+    // return string.repeat(num);
+    let sentence = '';
+    for (let i = 0; i < num; i++) {
+      sentence += string;
+    }
+    return sentence;
+  }
 };
 
 // Do not edit below this line
