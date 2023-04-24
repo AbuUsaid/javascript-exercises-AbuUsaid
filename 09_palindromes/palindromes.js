5f4110eff0f5c9d42751338da406056793c1b1a4
AbuUsaid <usaidabu321@gmail.com>
2023-04-25 00:08:21 +0530

    test pass 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  //   let reversedStr = str.split('').reverse().join('');
+  //   return reversedStr === str;
 
+  let processedStr = str.toLowerCase().replace(/[^a-z]/g, '');
+  return processedStr === processedStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
